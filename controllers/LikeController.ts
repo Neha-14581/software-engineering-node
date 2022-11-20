8dc389a61ad7dd8cb4dcc4b81e4534a76f717159
DESKTOP-C0NKLQ5\18573 <ramachandra.n@northeastern.edu>
2022-11-20 18:55:19 -0500

    initial additions for dislike-button

--- controllers/LikeController.ts
+++ controllers/LikeController.ts
@@ -109,7 +109,6 @@ export default class LikeController implements LikeControllerI {
             .then(like => res.json(like));
     }
 
-    //bug ffixed
 
     /**
      * @param {Request} req Represents request from client, including the
