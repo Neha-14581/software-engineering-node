ebf5ff4a66e6f71bcae056731761fd41cdf4af02
DESKTOP-C0NKLQ5\18573 <ramachandra.n@northeastern.edu>
2022-12-06 01:19:26 -0500

    final merge

--- server.ts
+++ server.ts
@@ -1,4 +1,3 @@
-
 /**
  * @file Implements an Express Node HTTP server. Declares RESTful Web services
  * enabling CRUD operations on the following resources:
@@ -6,73 +5,78 @@
  *     <li>users</li>
  *     <li>tuits</li>
  *     <li>likes</li>
- *     <li>bookmarks</li>
- *     <li>follows</li>
- *     <li>messages</li>
+ *     <li>BookMark</li>
+ *     <li>Message</li>
+ *     <li>Follow</li>
  * </ul>
- *
+ * 
  * Connects to a remote MongoDB instance hosted on the Atlas cloud database
  * service
  */
 
+import express, {Request, Response} from 'express';
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
-import express, {Request, Response} from 'express';
-import mongoose = require("mongoose");
 import LikeController from "./controllers/LikeController";
-import BookmarkController from "./controllers/BookmarkController";
-import MessageController from "./controllers/MessageController";
-import FollowController from "./controllers/FollowController";
-
-
-const cors = require('cors')
+import DislikeController from "./controllers/DislikeController";
+import BookMarkController from "./controllers/BookMarkController"
+import FollowController from './controllers/FollowController';
+import MessageController from './controllers/MessageController';
+import AuthenticationController from "./controllers/AuthenticationController";
+import mongoose from "mongoose";
+var cors = require('cors')
 const session = require("express-session");
+
+// connect to database
+const connectionString = `mongodb+srv://meet:meet1234@cluster0.zntvi.mongodb.net/myFirstDatabase?retryWrites=true`;
+mongoose.connect(connectionString);
+
+mongoose.connection.on("error", function(error) {
+  console.log(error)
+})
+
+mongoose.connection.on("open", function() {
+  console.log("Connected to MongoDB Database")
+})
+
 const app = express();
 
+app.use(cors({
+  credentials: true,
+  origin: ["http://localhost:3000", 'https://dulcet-kitsune-7fa4f2.netlify.app', 'https://super-gelato-e19dbd.netlify.app']
 
-app.use(cors());
-app.use(express.json());
-
-
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    autoIndex: false,
-    maxPoolSize: 10,
-    serverSelectionTimeoutMS: 5000,
-    socketTimeoutMS: 45000,
-    family: 4
-}
-
-
-
-// const app = express();
+}));
+const SECRET = 'process.env.SECRET';
 let sess = {
-    secret: process.env.SECRET,
-    cookie: {
-        secure: false
-    }
+  secret: SECRET,
+  saveUninitialized: true,
+  resave: true,
+  cookie: {
+      secure: false
+  }
 }
-
 if (process.env.ENV === 'PRODUCTION') {
-    app.set('trust proxy', 1) // trust first proxy
-    sess.cookie.secure = true // serve secure cookies
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
 }
-
-
-mongoose.connect('mongodb+srv://NehaRamachandra:1234@cluster0.zmme2.mongodb.net/tuiter?retryWrites=true&w=majority');
-app.use(cors());
+app.use(session(sess))
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome to Foundation of Software Engineering!!!!'));
+app.get('/hello', (req, res) =>
+res.send('Hello World!'));
 
+app.get('/add/:a/:b', (req: Request, res: Response) =>
+    res.send(req.params.a + req.params.b));
+// create RESTful Web service API
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
-const likesController = LikeController.getInstance(app);
-const bookmarksController = BookmarkController.getInstance(app);
-const messageController = MessageController.getInstance(app);
+const likeController = LikeController.getInstance(app);
+const dislikeController = DislikeController.getInstance(app);
+const bookmarkController = BookMarkController.getInstance(app);
 const followController = FollowController.getInstance(app);
+const messageController = MessageController.getInstance(app);
+AuthenticationController(app);
+
 
 /**
  * Start a server listening at port 4000 locally
